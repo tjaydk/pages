b0b37fd8379c661209c92bb117cf0be38e01f369
tjaydkSchool <dennis.michael@jubii.dk>
2017-05-09 18:00:18 +0200

    added a unit test

---
+++ src/test/java/Tests/SomeTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Dennis
+ */
+public class SomeTest {
+
+    public SomeTest() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void iAmTrueTest() {
+        assertTrue(true);
+    }
+}
